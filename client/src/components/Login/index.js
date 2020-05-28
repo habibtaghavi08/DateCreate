7c729b04c4a8feb1ffba2fec1600b0412fea8326
Celeste Lewis <cdlewis42@gmail.com>
2020-05-27 21:09:32 -0400

    grabs input box information and redirects back to page

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -6,7 +6,7 @@ return(
 <Form>
   <Form.Group controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Control type="email" placeholder="Enter email" className = "input" />
     <Form.Text className="text-muted"> {props.name}
       We'll never share your email with anyone else. {props.travis}
     </Form.Text>
@@ -15,6 +15,7 @@ return(
   <Form.Group controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
     <Form.Control type="password" placeholder="Password" type = "text"
+    className = "input"
     inputRef={(input) => this.inputPassword = input}/>
   </Form.Group>
   <Form.Group controlId="formBasicCheckbox">
