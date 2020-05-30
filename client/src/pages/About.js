e90f4952c4782e79486c533779e1594cfa83abde
habibtaghavi08 <habibtaghavi08@gmail.com>
2020-05-30 00:08:56 -0500

    added soem structure to the about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -5,20 +5,37 @@ import { DropdownDivider } from "react-bootstrap/Dropdown";
 
 function About() {
     return (
-     <div> 
-         <div>
- <h4>If you’ve ever tried to plan a romantic night out with your special someone, or that perfect first date, or even a casual night out with friends, you know how hard it can be to choose a great restaurant or to agree on the perfect weekend activity. Introducing Date Create, an all-in-one automated date generator.</h4>
- </div>
+        <div className="main">
+        <main>
+            <Container fluid>
+                <Row>
+                    <Col size="md-12">
+                        <div className="about-us">
+                            <h1>DateCreate:</h1>
+
+ <div>
+ <h4>If you’ve ever tried to plan a romantic night out with your special someone, or that perfect first date, or even a casual night out with friends,
+      you know how hard it can be to choose a great restaurant or to agree on the perfect weekend activity. Introducing Date Create, 
+      an all-in-one automated date generator.</h4>
+ 
   
-  <div> 
-<h4>The Date Create app allows the user to pick from 8 different categories in order to generate a random date idea including a restaurant and activity suggestion. The user will be presented with the restaurant/activity details including directions. If users don’t like the generated date, they can use the shuffle button to generate a new idea.  Use the app to plan a date in advance or plan on the go with the mobile responsive app. Gone are the days of negotiating where to eat and what to do.</h4> 
+<div> 
+<h4>The Date Create app allows the user to pick from 8 different categories in order to generate a random date idea including a restaurant and 
+    activity suggestion. The user will be presented with the restaurant/activity details including directions. If users don’t like the generated 
+    date, they can use the shuffle button to generate a new idea.  Use the app to plan a date in advance or plan on the go with the mobile responsive
+    app. Gone are the days of negotiating where to eat and what to do.</h4> 
 </div>
 
 <div>
 <h4>With Date Create every date is an adventure.</h4>
 </div>
 
-</div>
+ </div>
+</main>
+ </div>
+                        </Col>
+                          
+                              
 
 
 
